fb375401871efff68ffa07d5b1b8373b4dc5cb04
jiayng01 <92442809+jiayng01@users.noreply.github.com>
2022-10-25 01:05:58 +0800

    Revert "School info filter schools"

--- school-info/src/Pages/Error.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../PagesCSS/Error.css";
-
-function Error() {
-  return <p className="error-style">Ooops! This page doesn't seem to exist!</p>;
-}
-
-export default Error;
